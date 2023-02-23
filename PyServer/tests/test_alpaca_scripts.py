2e07582b62baaefc61e4904fae71539c61c41477
Regal <98935983+devRegal@users.noreply.github.com>
2023-02-23 11:05:29 -0800

    PyServer v02

--- PyServer/tests/test_alpaca_scripts.py
+++ PyServer/tests/test_alpaca_scripts.py
@@ -1,42 +1,24 @@
 import unittest
+import os
 import sys
-sys.path.append('..')
+import pandas as pd
 
-from alpaca_script import (get_position, get_signal, get_pause, get_sma, get_bars)
 
-class TestMain(unittest.TestCase):
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-    def test_get_position(self):
-        # Test that get_position returns 0 if no position exists
-        self.assertEqual(get_position("DOGE/USD"), 0)
-        # Test that get_position returns a float if a position exists
-        self.assertIsInstance(get_position("DOGEUSD"), float)
 
-    def test_get_signal(self):
-        # Test that get_signal returns True if the fast moving average is greater than the slow moving average
-        self.assertTrue(get_signal([10, 20, 30], [5, 15, 25]))
-        # Test that get_signal returns False if the fast moving average is less than the slow moving average
-        self.assertFalse(get_signal([30, 20, 10], [25, 15, 5]))
-        # Test that get_signal returns False if the fast moving average is equal to the slow moving average
-        self.assertFalse(get_signal([10, 20, 30], [10, 20, 30]))
+from alpaca_script import get_pause, get_position, get_sma, get_signal, get_bars
 
-    def test_get_pause(self):
-        # Test that get_pause returns an integer
-        self.assertIsInstance(get_pause(), int)
+NORMALIZED_SYMBOL = "DOGEUSD"
+SYMBOL = "DOGE/USD"
+
+class TestAlpacaScript(unittest.TestCase):
+    def test_get_position_no_positions(self):
+        self.assertEqual(get_position("AAPL"), 0)
 
     def test_get_sma(self):
-        # Test that get_sma returns a pandas Series object
-        self.assertIsInstance(get_sma([10, 20, 30], 2), pd.Series)
-        # Test that get_sma calculates the moving average correctly
-        self.assertTrue((get_sma([10, 20, 30], 2) == pd.Series([np.nan, 15, 25])).all())
+        series = pd.Series([1, 2, 3, 4, 5])
+        self.assertEqual(get_sma(series, 3).iloc[-1], 4)
 
-    def test_get_bars(self):
-        # Test that get_bars returns a pandas DataFrame object
-        self.assertIsInstance(get_bars("DOGE/USD"), pd.DataFrame)
-        # Test that get_bars adds two new columns for the fast and slow moving averages
-        bars = get_bars("DOGE/USD")
-        self.assertIn('sma_fast', bars.columns)
-        self.assertIn('sma_slow', bars.columns)
 
-if __name__ == '__main__':
-    unittest.main()
+
