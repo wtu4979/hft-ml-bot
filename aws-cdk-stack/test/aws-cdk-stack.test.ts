1702aaf0ee0a36d1d4a5c74da8990ccef1bbe9a8
Regal <casian.geovanny2015@gmail.com>
2023-02-23 16:11:07 -0800

    AWS CDK Code added

---
+++ aws-cdk-stack/test/aws-cdk-stack.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsCdkStack from '../lib/aws-cdk-stack-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-cdk-stack-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsCdkStack.AwsCdkStackStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
