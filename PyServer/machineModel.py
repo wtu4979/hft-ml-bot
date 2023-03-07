e43fbf4eca8b485cb55f1acd66a2742c33f5c4d6
Regal <98935983+devRegal@users.noreply.github.com>
2023-03-06 21:56:40 -0800

    HS-63
    Created a basic ml model to integrate with the data collection

---
+++ PyServer/machineModel.py
@@ -0,0 +1,53 @@
+import pandas as pd
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+
+# load the CSV data
+df = pd.read_csv("btc_data.csv")
+
+# convert the CloseTime column to a datetime object
+df["CloseTime"] = pd.to_datetime(df["CloseTime"])
+
+# create a new column for the target variable (i.e., the label we want to predict)
+df["Target"] = np.where(df["ClosePrice"].shift(-1) > df["ClosePrice"], 1, 0)
+
+# drop the last row since it has no target value
+df.drop(df.tail(1).index, inplace=True)
+
+# set the input features and target variable
+X = df.drop(["Target", "CloseTime"], axis=1)
+y = df["Target"]
+
+# split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# create the random forest classifier
+rf = RandomForestClassifier(n_estimators=100, random_state=42)
+
+# fit the model on the training data
+rf.fit(X_train, y_train)
+
+
+# define the function to make predictions
+def make_prediction(current_price):
+    # create a new dataframe with the current price as the only feature
+    new_data = pd.DataFrame({
+        "OpenPrice": [current_price],
+        "HighPrice": [current_price],
+        "LowPrice": [current_price],
+        "ClosePrice": [current_price],
+        "Volume": [0],
+        "QuoteVolume": [0]
+    })
+
+    # predict the target value using the model
+    prediction = rf.predict(new_data)[0]
+
+    # map the prediction to a string
+    if prediction == 1:
+        return "Buy"
+    elif prediction == 0:
+        return "Hold"
+    else:
+        return "Sell"
