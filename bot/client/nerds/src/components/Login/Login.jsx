56ec8818ca228d836986204b9f0eefd9e394c370
Your Name <you@example.com>
2023-03-11 23:27:34 -0800

    [HS-70] Our Login and Signup are connected to our DB via our inex.js file

--- bot/client/nerds/src/components/Login/Login.jsx
+++ bot/client/nerds/src/components/Login/Login.jsx
@@ -3,15 +3,29 @@ import React, {useState} from "react";
 import axios from "axios";
 
 const Login = () => {
+  const [email, setemail] = useState('');
+  const [password, setpassword] = useState('');
+
+  const login = () =>{
+    axios.post("http://localhost:3001/Signup",{
+      email: email,
+      password: password
+    }).then((response) => {
+      console.log(response);
+    });
+  };
 
   return (
     <div className="login">
         <h1>Login</h1>
         <form>
-            <input type={"email"} placeholder={'Email'} 
-            />
-            <input type={'password'} placeholder={'Password'}/>
-            <button type={'submit'} >Login</button>
+            <input type={"email"} placeholder={'Email'} onChange={(e) => {
+              setemail(e.target.value);
+            }}/>
+            <input type={'password'} placeholder={'Password'} onChange= { (e) => {
+              setpassword(e.target.value);
+            }}/>
+            <button type={'submit'} onClick={login}>Login</button>
         </form>
     </div>
   )
--- bot/client/nerds/src/components/Signup/Signup.jsx
+++ bot/client/nerds/src/components/Signup/Signup.jsx
@@ -2,18 +2,40 @@ import "./Signup.css"
 import React, {useState} from "react";
 import axios from "axios";
 
+const Signup = () => {
+  const [fullnameReg, setfullnameReg] = useState('');
+  const [passwordReg, setpasswordReg] = useState('');
+  const [emailReg, setemailReg] = useState('');
+  const [confpasswordReg, setconfpasswordReg] = useState('');
+  const [api_keyReg, setapi_keyReg] =useState('');
 
+  const Signup = () => {
+    axios.post('http://localhost:3001/Signup', {
+      fullname: fullnameReg,
+      email: emailReg,
+      password: passwordReg,
+      confpassword: confpasswordReg,
+      api_key: api_keyReg
+    }).then((response) =>{
+      console.log(response);
+    });
+  };
 
   return (
     <div className="Signup">
       <h1>Signup</h1>
       <form>
-            <input type={"text"} placeholder={'Fullname'}/ >
-            <input type={"email"} placeholder={'Email'} />
-            <input type={'password'} placeholder={'Password'} />
-            <input type={'password'} placeholder={'ConfirmPassword'} />
-            <input type={'text'} placeholder={'API-Secret-Keys'}/>
-            <button type={'submit'}  >SignUp</button>
+            <input type={"text"} placeholder={'Fullname'} onChange={(e)=>{
+              setfullnameReg(e.target.value);}}/>
+            <input type={"email"} placeholder={'Email'} onChange= {(e)=>{
+              setemailReg(e.target.value);}}/>
+            <input type={'password'} placeholder={'Password'} onChange= {(e)=>{
+              setpasswordReg(e.target.value);}}/>
+            <input type={'password'} placeholder={'ConfirmPassword'} onChange= {(e)=>{
+              setconfpasswordReg(e.target.value);}}/>
+            <input type={'text'} placeholder={'API-Secret-Keys'}onChange= {(e)=>{
+              setapi_keyReg(e.target.value);}}/>
+            <button type={'submit'} onClick={Signup} >SignUp</button>
       </form>
     </div>
   )
