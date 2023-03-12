bb83ccb300e272f3f5bbbcc4cc9a8735e966b1f4
Sergiom1535 <71371817+Sergiom1535@users.noreply.github.com>
2023-03-11 23:40:39 -0800

    Update index.js

--- bot/server/index.js
+++ bot/server/index.js
@@ -12,7 +12,43 @@ const db = mysql.createPool({
     password: 'Yeyo!535',
     database: 'nerds'
 });
+app.post('/Signup',(req, res) =>{
 
+    const fullname = req.body.fullname;
+    const password = req.body.password;
+    const email = req.body.email;
+    const confpassword = req.body.confpassword;
+    const api_key = req.body.api_key
+
+    db.query(
+    "INSERT INTO login (fullname,email,pasword,confpassword,api_key) VALUES (?,?,?,?,?)" , 
+    [fullname, email,password,confpassword,api_key],
+    (err, result)=> {
+        if (err) {
+            res.send({err: err})
+        }
+        if (result) {
+            res.send(result);
+        } else{
+            res.send({message: "Wrong email/password combination!"});
+        }        
+    
+    }
+    );
+});
+
+app.post('/Login', (req,res) =>{
+    const email = req.body.email;
+    const password = req.body.password;
+
+    db.query(
+        "Select * FROM login Where email = ? AND pasword = ?",
+        [email, password],
+        (err, result) => {
+            console.log(err);
+        }
+    );
+});
 app.listen(3001, () => {
     console.log('running on port 3001');
 });[]
