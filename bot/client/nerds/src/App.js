2cf8df63120eacd52a6134bdb9507295dddedb2d
Your Name <you@example.com>
2023-05-04 22:53:03 -0700

    update

--- bot/client/nerds/src/App.js
+++ bot/client/nerds/src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Lscontainer from "./components/LoginSignupContainer/Lscontainer.jsx";
 import Dashboard from "./components/Dashboard/Dashboard";
+import About from "./components/About/About";
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous"></link>
 
 class App extends Component {
@@ -13,6 +14,7 @@ class App extends Component {
       <Routes>
         <Route path='/' element={ <Lscontainer />}/>
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/about" element={<About />}/>
       </Routes>
       </Router>
     </div>
